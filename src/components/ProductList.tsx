287ca895d2cc6f9b601c8a251f64adc22768ccc3
Lucas Otaño <lucasmatiasota@gmail.com>
2024-03-28 23:59:47 -0300

    feat: add graphql-codegen for typing

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client';
+import { GetAllProductsQuery } from '../generated/graphql';
 import { GET_ALL_PRODUCTS } from '../graphql/queries';
 
 export function ProductList() {
-  const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
+  const { loading, error, data } =
+    useQuery<GetAllProductsQuery>(GET_ALL_PRODUCTS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -11,7 +13,7 @@ export function ProductList() {
     <div>
       <h2>Products</h2>
       <ul>
-        {data.products.items.map((product) => (
+        {data?.products.items.map((product) => (
           <li key={product.id}>
             {product.name} - ${product.variants[0].price / 100}
           </li>
