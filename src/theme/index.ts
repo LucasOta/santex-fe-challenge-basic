e0bb14eff4980213b900aeeb9d6bd7798501bba3
Lucas Otaño <lucasmatiasota@gmail.com>
2024-07-07 22:03:06 -0300

    feat: use Santex's colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -10,23 +10,27 @@ export interface Theme {
     warning: string;
     info: string;
     light: string;
+    gray: string;
     dark: string;
+    black: string;
   };
 }
 
 const darkTheme: Theme = {
   colors: {
-    bg: '#121212',
+    gray: '#8b919a',
+    bg: '#111',
     fg: '#f8f9fa',
     text: '#FFFFFF',
-    primary: '#0d6efd',
-    secondary: '#6c757d',
+    primary: '#db76ec',
+    secondary: '#f8674c',
     success: '#198754',
     danger: '#dc3545',
     warning: '#ffc107',
     info: '#0dcaf0',
     light: '#f8f9fa',
     dark: '#343a40',
+    black: '#000',
   },
 };
 
